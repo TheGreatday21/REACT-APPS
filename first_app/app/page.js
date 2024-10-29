0600b95cd40615e08c2945542735622bb447ac24
TheGreatday21 <kaizzielijah@gmail.com>
2024-10-29 21:08:00 +0300

    simple

--- first_app/app/page.js
+++ first_app/app/page.js
@@ -2,15 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 function drum(){
-return <html>
-  <head>
-    <title>My website </title>
-  </head>
-  <body>
-    <div>
-      <p style = "color : #1c18ac" >Iam the head not the tail </p>
-    </div>
-  </body>
-</html>
+return <div>
+    <p style = "color : #1c18ac" >Iam the head not the tail </p>
+</div>
+
 }
 export default drum;
