086d72c08243d77f5c5b6f6e86966dca4995f1a7
TheGreatday21 <kaizzielijah@gmail.com>
2024-10-29 20:51:20 +0300

    testing_out_html

--- first_app/app/page.js
+++ first_app/app/page.js
@@ -2,8 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 function drum(){
-return <div>
-  <p>Iam Kawooma Elijah the greatest technician thats ever lived</p>
-</div>
+return <html>
+  <head>
+    <title>My website </title>
+  </head>
+  <body>
+    <div>
+      <p style = "color : #1c18ac" >Iam the head not the tail </p>
+    </div>
+  </body>
+</html>
 }
 export default drum;
