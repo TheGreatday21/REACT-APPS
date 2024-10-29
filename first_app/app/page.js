79d827588a04992adb70abed1d754854c03a5226
TheGreatday21 <kaizzielijah@gmail.com>
2024-10-29 21:35:39 +0300

    new_start

---
+++ first_app/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+function Home() {
+  return <div>
+    <p>God loves me and you too</p>
+  </div>
+};
+export default Home;
