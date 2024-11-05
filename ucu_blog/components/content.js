3b949419cb01627341ad5f65d142f447a244df6f
TheGreatday21 <kaizzielijah@gmail.com>
2024-11-05 09:20:45 +0300

    getting_there

--- ucu_blog/components/content.js
+++ ucu_blog/components/content.js
@@ -1,7 +1,9 @@
 function content(){
     return(
         <div>
-            <p>lorem</p>
+            <p>God is good all the time </p>
+            <p>If he aint then you lying , In the rain in the shine </p>
+            <p>God is good all the time </p>
         </div>
     );
 }
--- ucu_blog/src/app/page.js
+++ ucu_blog/src/app/page.js
@@ -1,18 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import content from "../../components/content"
-import title from "../../components/title"
-
-
 function Elijah(){
+  
   return( 
-  <div>
-    <h1>TecH Savvy Uganda </h1>
-    <p>This is sample code </p>
-    <title />
-    <p>This is the start of our semester project</p>
-    <content />
-  </div>
-);
-}
+    <div>
+      <h1>TecH Savvy Uganda </h1>
+      <p>This is sample code </p>
+      <title />
+      <p>This is the start of our semester project</p>
+      <content />
+    </div>
+)};
 export default Elijah;
